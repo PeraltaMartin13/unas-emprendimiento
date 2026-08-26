b453ca666ad572946366f2eaabe0bfe7738a337a
PeraltaMartin13 <martinperaltasmurra13@gmail.com>
2026-08-26 00:26:54 -0300

    Un poco de diseño

--- app/components/ServiceCard.tsx
+++ app/components/ServiceCard.tsx
@@ -5,9 +5,9 @@ type ServiceCardProps ={
 
 export default function ServiceCard ({title, description}: ServiceCardProps){
     return(
-        <div className="bg-white p-6 rounded-2xl">
-            <h3>{title}</h3>
-            <p>{description}</p>
+        <div className="bg-zinc-50 p-6 rounded-2xl border border-zinc-200">
+            <h3 className="text-xl font-semibold text-zinc-950">{title}</h3>
+            <p className="text-zinc-600">{description}</p>
             
         </div>
     );
