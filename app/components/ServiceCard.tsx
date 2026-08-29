c9c50484683935d9adf893d208446db0646f19c7
PeraltaMartin13 <martinperaltasmurra13@gmail.com>
2026-08-28 22:32:12 -0300

    grid y mas cosas

--- app/components/ServiceCard.tsx
+++ app/components/ServiceCard.tsx
@@ -1,13 +1,18 @@
+import Image from "next/image";
+
 type ServiceCardProps ={
     title: string;
+    image: string;
     description: string;
 };
 
-export default function ServiceCard ({title, description}: ServiceCardProps){
+
+export default function ServiceCard ({title, image, description}: ServiceCardProps){
     return(
-        <div className="bg-zinc-50 p-6 rounded-2xl border border-zinc-200">
+        <div className="bg-zinc-50 p-6 rounded-2xl text-center border border-zinc-200">
             <h3 className="text-xl font-semibold text-zinc-950">{title}</h3>
-            <p className="text-zinc-600">{description}</p>
+            <Image className="mx-auto" src={image} alt={title} width={600} height={400}></Image>
+            <p className="text-zinc-600 text-left py-2">{description}</p>
             
         </div>
     );
--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -1,21 +1,23 @@
 import ServiceCard from "./ServiceCard";
 
 const services = [
-    {title: "Esmaltado Semi permanente", description:"Esmaltado Semi Permanente sobre tu uña natural." },
-    {title: "Caping Acrigel(Diseño elaborado)", description:"Refuerzo con poligel sobre largo natural + diseño a tu gusto." },
-    {title: "Esculpidas En Acrilico", description:"Alargá y dale tu forma favorita a tus uñas ." },
-    {title: "Soft Gel", description:"Alarga tus uñas con los tips de gel, se adhieren a tu uña natural logrando una extensión más simple y un resultado duradero y delicado." },
-    {title: "Retiro de Semi permanente", description:"Opcion unicamente para retiros de otros salones! o si desea retiro y no aplicarse un nuevo servicio." },
-    {title: "Retiro de Acrilico/Acrigel", description:"Opcion unicamente para retiros de otros salones! o si desea retiro y no aplicarse un nuevo servicio." },
+    {title: "Caping Acrigel", description:"Refuerzo con poligel sobre largo natural + diseño a tu gusto.", image: "/media/capping-acrigel-1.jpeg" },
+    {title: "Caping Acrilico", description:"Refuerzo con poligel sobre largo natural + diseño a tu gusto.", image: "/media/capping-acrilico-1.jpeg" },
+    {title: "Esculpidas En Acrilico", description:"Alargá y dale tu forma favorita a tus uñas .", image: "/media/esculpidas-1.jpeg" },
+    {title: "Esmaltado Semi permanente", description:"Esmaltado Semi Permanente sobre tu uña natural.", image: "/media/semi-permanente.jpeg" },
+    {title: "Soft Gel", description:"Alarga tus uñas con los tips de gel, se adhieren a tu uña natural logrando una extensión más simple y un resultado duradero y delicado.", image: "/media/semi-permanente.jpeg" },
+    {title: "Retiro de Semi permanente", description:"Opcion unicamente para retiros de otros salones! o si desea retiro y no aplicarse un nuevo servicio.", image: "/media/semi-permanente.jpeg" },
+    {title: "Retiro de Acrilico/Acrigel", description:"Opcion unicamente para retiros de otros salones! o si desea retiro y no aplicarse un nuevo servicio.", image: "/media/semi-permanente.jpeg" },
 ];
 
 export default function Services(){
     return(
-        <section>
+        <section className="gap-8 py-14 px-16 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
             {services.map((service) => (
                 <ServiceCard 
                 key={service.title}
                 title={service.title}
+                image={service.image}
                 description={service.description}
                 />
             ))}
