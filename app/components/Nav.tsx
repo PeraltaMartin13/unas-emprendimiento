fc3baf38370aa011732ae5aecbbdaad24a242d2d
PeraltaMartin13 <martinperaltasmurra13@gmail.com>
2026-09-05 16:09:36 -0300

    Agregue menu hambursea al mobil con useState

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,16 +1,25 @@
+"use client"
+
 import Image from "next/image"
+import { useState } from "react"
 
 
-export default function Nav(){
-    return(
+
+export default function Nav() {
+    const [menuDesplegado, setMenuDesplegado] = useState(false)
+
+    return (
         <nav className="flex w-full items-center sticky top-0 px-10 justify-between bg-violet-400">
-            <Image src="/media/logo-haka.png" alt="logo Haka"  width={90} height={60}></Image>
-            <div className="flex gap-x-24 text-2xl font-bold">
-                <a href="https://calendico.com/haka/Haka">Reservar Un Turno</a>
-                <a href="#Services">Servicios</a>
-                <a href="#Testimonios">Ustedes</a>
-                <a href="#Ubicacion">Donde Estoy?</a>
-                <a href="#SobreMi">Un Poco Sobre mi</a>
+            <Image src="/media/logo-haka.png" alt="logo Haka" width={90} height={60}></Image>
+            <div className="flex gap-5" >
+                <button className="md:hidden" onClick={() => setMenuDesplegado(!menuDesplegado)}>☰</button>
+                <div className={`${menuDesplegado ? "flex flex-col" : "hidden"} gap-y-4 text-base font-bold md:flex md:flex-row md:text-2xl md:gap-x-10`}>
+                    <a href="https://calendico.com/haka/Haka">Reservar Un Turno</a>
+                    <a href="#Services">Servicios</a>
+                    <a href="#Testimonios">Ustedes</a>
+                    <a href="#Ubicacion">Mi ubicacion</a>
+                    <a href="#SobreMi">Un Poco Sobre mi</a>
+                </div>
             </div>
         </nav>
     )
